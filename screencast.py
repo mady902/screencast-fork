d56fe44c3773ff21ce34b8413c5885d59438a7d4
rfabbri <rfabbri@ubuntu.(none)>
2012-03-24 05:05:06 -0300

    agora com todas as gambis, estah rolando. Talvez fazer um concatenador para dar conta do rolo

--- screencast.py
+++ screencast.py
@@ -12,19 +12,20 @@ while 1:
     pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
     pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
     if (pb != None):
-        name="screenshot%03d.png" % (i,)
+        name="screenshot%05d.png" % (i,)
         pb.save(name ,"png")
         #print "Screenshot saved to %s." % (name,)
         lag=(time.time()-anterior)
         #correction = DUR - lag
         dur2 = DUR -lag
-        if dur2 < 0:
+        j=2
+        while (dur2 < 0):
             i+=1
-            name2="screenshot%03d.png" % (i,)
+            name2="screenshot%05d.png" % (i,)
             os.system('cp %s %s' % (name,name2))
 
             lag=(time.time()-anterior)
-            dur2=2*DUR -lag
+            dur2=j*DUR -lag; j+=1
 
         #print "lag=%.3f dur2=%.3f tot=%.3f" % (lag, dur2, lag+dur2)
         time.sleep(dur2)
