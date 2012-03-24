9ee1067ca9261b603ce3f272435e761ecebdacb7
rfabbri <rfabbri@ubuntu.(none)>
2012-03-24 01:28:03 -0300

    arrumado o cara, primeira versao que realmente roda

--- makeVideo.sh
+++ makeVideo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ls -tr > arquivos.txt
-mencoder mf://@arquivos.txt -mf w=400:h=200:fps=1:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o output.avi
+mencoder mf://@arquivos.txt -mf w=400:h=200:fps=1:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o output.avi -audiofile teste.wav
+#mencoder output.avi -o output2.avi -ovc copy -oac mp3lame -audiofile teste.wav
