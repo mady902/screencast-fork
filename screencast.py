7c83a9c181cd33801bb728cfb99084d39170d0e1
rfabbri <rfabbri@ubuntu.(none)>
2012-03-24 02:58:34 -0300

    tirei os prints e aumentei para 2 o fps

--- screencast.py
+++ screencast.py
@@ -1,6 +1,6 @@
 import time, gtk.gdk, os, thread
 
-DUR=1
+DUR=0.5
 
 i=1
 thread.start_new_thread( os.system, ('arecord -f cd -c 1 -t wav > teste.wav',)  )
@@ -8,13 +8,13 @@ anterior=time.time()
 while 1:
     w = gtk.gdk.get_default_root_window()
     sz = w.get_size()
-    print "The size of the window is %d x %d" % sz
+    #print "The size of the window is %d x %d" % sz
     pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
     pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
     if (pb != None):
         name="screenshot%03d.png" % (i,)
         pb.save(name ,"png")
-        print "Screenshot saved to %s." % (name,)
+        #print "Screenshot saved to %s." % (name,)
         lag=(time.time()-anterior)
         #correction = DUR - lag
         dur2 = DUR -lag
